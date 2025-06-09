bb8aef2b03ebb66652d46938f3db4546a7e60b26
santhosh-2504 <santhoshanantha58@gmail.com>
2025-06-09 21:36:41 +0530

    Changes

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -82,7 +82,7 @@ export const ThemeToggle = () => {
     <button
       onClick={toggleTheme}
       className={cn(
-        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+        "fixed max-sm:hidden top-5 right-5 z-50 p-1 rounded-full transition-colors duration-300",
         "focus:outline-none"
       )}
     >
